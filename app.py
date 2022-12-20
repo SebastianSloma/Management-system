585fd0c9a39150864a218c3f9397471862894711
Sebastian <sebastiansloma@gmail.com>
2022-12-20 20:33:19 +0100

    create window

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
 from tkinter import *
 from tkinter import ttk
 from PIL import Image, ImageTk
+
+
+class Managment:
+    def __init__(self, root):
+        self.root = root
+        self.root.geometry('1500x750+0+0')
+        self.root.title('SYSTEM MANAGMENT')
+
+
+if __name__ == '__main__':
+    root = Tk()
+    obj = Managment(root)
+    root.mainloop()
