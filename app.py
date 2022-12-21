4c2aa4605219aa6392e95113481516fb4b6c2a8d
Sebastian <sebastiansloma@gmail.com>
2022-12-21 09:55:24 +0100

    create app title

--- app.py
+++ app.py
@@ -6,8 +6,11 @@ from PIL import Image, ImageTk
 class Managment:
     def __init__(self, root):
         self.root = root
-        self.root.geometry('1500x750+0+0')
-        self.root.title('SYSTEM MANAGMENT')
+        self.root.geometry('1300x750+0+0')
+        self.root.title('EXPENSE SYSTEM MANAGMENT')
+
+        label_title=Label(self.root,text='EXPENSE SYSTEM MANAGMENT',font=('modern',40,'bold'), bg='black', fg='white')
+        label_title.place(x=0,y=0,width=1300,height=70)
 
 
 if __name__ == '__main__':
