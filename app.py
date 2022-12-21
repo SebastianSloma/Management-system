3c9ad5fd187bafdd82d2d3e664630713e71b32e2
Sebastian <sebastiansloma@gmail.com>
2022-12-21 09:55:50 +0100

    .

--- app.py
+++ app.py
@@ -9,8 +9,9 @@ class Managment:
         self.root.geometry('1300x750+0+0')
         self.root.title('EXPENSE SYSTEM MANAGMENT')
 
-        label_title=Label(self.root,text='EXPENSE SYSTEM MANAGMENT',font=('modern',40,'bold'), bg='black', fg='white')
-        label_title.place(x=0,y=0,width=1300,height=70)
+        label_title = Label(self.root, text='EXPENSE SYSTEM MANAGMENT', font=(
+            'modern', 40, 'bold'), bg='black', fg='white')
+        label_title.place(x=0, y=0, width=1300, height=70)
 
 
 if __name__ == '__main__':
